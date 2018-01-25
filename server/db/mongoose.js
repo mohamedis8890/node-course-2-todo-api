2571686a5603cbdbabdd4d51cee7fdc5e88820b5
mohamedis8890 <mohamed.is8890@gmail.com>
2018-01-26 00:18:48 +0200

    Setup for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://admin:admin@gttodocluster-shard-00-00-zynaz.mongodb.net:27017,gttodocluster-shard-00-01-zynaz.mongodb.net:27017,gttodocluster-shard-00-02-zynaz.mongodb.net:27017/test?ssl=true&replicaSet=GTTodoCluster-shard-0&authSource=admin');
 
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(bodyParser.json());
@@ -52,8 +53,8 @@ app.get('/todos/:id', (req, res) => {
         });
 });
 
-app.listen(3000, () => {
-    console.log('App started on port 3000');
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
 });
 
 module.exports = { app };
