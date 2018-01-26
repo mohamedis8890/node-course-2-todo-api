b78d74a2383d3181e6656c0818fbd46c49f54906
mohamedis8890 <mohamed.is8890@gmail.com>
2018-01-26 15:04:37 +0200

    Added CORS

--- server/server.js
+++ server/server.js
@@ -11,6 +11,19 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
